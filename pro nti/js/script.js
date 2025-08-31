89af683b1d487cf51a8cc2fa16b5b02100b61e21
Sara Mahmoud <mahmoudsara778@gmail.com>
2025-08-31 11:24:32 +0300

    Add files via upload

---
+++ pro nti/js/script.js
@@ -0,0 +1,13 @@
+
+var name ="Sara";
+var gpa =3.3;
+ function yourResult(){
+    console.log(`Hello ${name} ,Gpa: ${gpa}`)
+ }
+ yourResult()
+
+ if(gpa < 3){
+   console.log(`You're failed`)
+ }else{
+   console.log(`You're pass`)
+ }
